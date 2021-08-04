3261d868203f5d19fb77c81446f2369506769009
alphagirl-tech <76767240+alphagirl-tech@users.noreply.github.com>
2021-08-04 19:41:09 +0530

    Create bfs_h.h

---
+++ BFS/Unpartitioned Version/bfs_h.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define CL_HPP_CL_1_2_DEFAULT_BUILD
+#define CL_HPP_TARGET_OPENCL_VERSION 120
+#define CL_HPP_MINIMUM_OPENCL_VERSION 120
+#define CL_HPP_ENABLE_PROGRAM_CONSTRUCTION_FROM_ARRAY_COMPATIBILITY 1
+
+#define V 4720
+#define E 27444
+void shortest(int *col, int *nzr, int *visited, int *q, int src);
